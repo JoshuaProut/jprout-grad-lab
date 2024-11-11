4e9df63137f10a7027f2b80a6dc7d522376166a7
Josh Prout <jnp207@exeter.ac.uk>
2024-11-11 16:39:43 +0000

    Move bucket and table reference to .tfbackend files

--- terraform/layers/base/backend.tf
+++ terraform/layers/base/backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = "937405989913-eu-west-2-grad-lab-1-tf-state"
-    key            = "dev/base/terraform.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "937405989913-eu-west-2-grad-lab-1-tf-state"
-  }
+  backend "s3" {}
 }
