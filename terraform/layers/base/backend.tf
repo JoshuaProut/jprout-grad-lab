7dd1c621e4a56e0b45e3552d047affc1ed64bcaa
Josh Prout <jnp207@exeter.ac.uk>
2024-11-11 16:15:48 +0000

    Add dynamoDB lock table reference to backends

--- terraform/layers/base/backend.tf
+++ terraform/layers/base/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "937405989913-eu-west-2-grad-lab-1-tf-state"
-    key    = "dev/base/terraform.tfstate"
-    region = "eu-west-2"
+    bucket         = "937405989913-eu-west-2-grad-lab-1-tf-state"
+    key            = "dev/base/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "937405989913-eu-west-2-grad-lab-1-tf-state"
   }
 }
