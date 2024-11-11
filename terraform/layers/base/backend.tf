95ed7b04e0cb042a8db84b7549086f5cb6f0310a
Josh Prout <jnp207@exeter.ac.uk>
2024-11-11 16:10:23 +0000

    Set up S3 backend

--- terraform/layers/base/backend.tf
+++ terraform/layers/base/backend.tf
@@ -1,3 +1,7 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket = "937405989913-eu-west-2-grad-lab-1-tf-state"
+    key    = "dev/base/terraform.tfstate"
+    region = "eu-west-2"
+  }
 }
