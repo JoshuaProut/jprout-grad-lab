449df78a87012e807ab35f38f18e552baca87e1a
Josh Prout <jnp207@exeter.ac.uk>
2024-11-18 11:40:57 +0000

    Dynamic naming for role and IAM policy

--- terraform/layers/app/iam.tf
+++ terraform/layers/app/iam.tf
@@ -7,9 +7,13 @@ output "s3_name" {
   value = module.base.s3_webfiles_arn
 }
 
+locals {
+  resource_prefix = "${var.project}-${var.environment}"
+}
+
 // Create IAM role
 resource "aws_iam_role" "s3_webfiles_access" {
-  name = "s3_webfiles_access"
+  name = "${local.resource_prefix}_s3_webfiles_access"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -28,7 +32,7 @@ resource "aws_iam_role" "s3_webfiles_access" {
 
 // Create IAM policy allowing access to S3 bucket 
 resource "aws_iam_policy" "s3_access_policy" {
-  name        = "S3AccessPolicy"
+  name        = "${local.resource_prefix}_s3_access_policy"
   description = "Allows EC2 instance to access web files S3 bucket"
 
   policy = jsonencode({
