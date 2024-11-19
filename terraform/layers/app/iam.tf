deb35cfd95b57f550ce8dd849e79b38ad78cecf6
Josh Prout <jnp207@exeter.ac.uk>
2024-11-19 17:14:39 +0000

    Move data blocks to seperate file to avoid code repetition

--- terraform/layers/app/iam.tf
+++ terraform/layers/app/iam.tf
@@ -1,22 +1,3 @@
-/* Find base module
-module "base" {
-  source = "../base"
-}
-
-output "s3_name" {
-  value = module.base.s3_webfiles_arn
-}
-
-*/
-
-
-// Get S3 bucket data
-data "aws_s3_bucket" "s3_webfiles" {
-  bucket = "${local.resource_prefix}-s3-webfiles"
-}
-
-
-
 locals {
   resource_prefix = "${var.project}-${var.environment}"
 }
