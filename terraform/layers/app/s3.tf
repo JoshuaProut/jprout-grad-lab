117d08bb3d0807c994508ea893b680db72651183
Josh Prout <jnp207@exeter.ac.uk>
2024-11-19 17:15:18 +0000

    Use bucket ID from data source instead of hardcoded name

--- terraform/layers/app/s3.tf
+++ terraform/layers/app/s3.tf
@@ -1,6 +1,7 @@
 
-resource "aws_s3_object" "object" {
-  bucket = "${local.resource_prefix}-s3-webfiles"
+
+resource "aws_s3_object" "s3_webfiles_object" {
+  bucket = data.aws_s3_bucket.s3_webfiles.id
   key    = "index.html"
   source = "src/index.html"
 }
