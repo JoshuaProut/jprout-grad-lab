cd42094958ad80a0ad9f098f88f60543c0133592
Josh Prout <jnp207@exeter.ac.uk>
2024-11-20 09:50:10 +0000

    Create instance security group and launch template

--- terraform/layers/app/asg.tf
+++ terraform/layers/app/asg.tf
@@ -9,7 +9,38 @@ data "aws_ami" "amazon_linux_2" {
   }
 }
 
-output "latest_amazon_linux" {
-  value = data.aws_ami.amazon_linux_2.id
+// EC2 security group
+
+resource "aws_security_group" "web_instances_sg" {
+  name        = "${local.resource_prefix}-web-instance-sg"
+  description = "Allow HTTP traffic connection to the load balancer"
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_http_in" {
+  security_group_id = aws_security_group.web_instances_sg.id
+
+  referenced_security_group_id = module.aws_security_group.alb_sg.id
+
+  from_port   = 80
+  to_port     = 80
+  ip_protocol = "tcp"
+
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_out" {
+  security_group_id = aws_security_group.web_instances_sg.id
+
+  referenced_security_group_id = module.aws_security_group.alb_sg.id
+
+  from_port   = 0
+  to_port     = 0
+  ip_protocol = "tcp"
+}
+
+
+resource "aws_launch_template" "web_instances" {
+  name                   = "${local.resource_prefix}-launch-template"
+  image_id               = data.aws_ami.amazon_linux_2.image_id
+  vpc_security_group_ids = [aws_security_group.web_instances_sg]
+
+}
