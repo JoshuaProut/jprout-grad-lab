1d98a70d30c3e60347cf05fdd675ad70d668b387
Josh Prout <jnp207@exeter.ac.uk>
2024-12-17 11:12:10 +0000

    Add ownership controls

--- terraform/layers/deploy/s3.tf
+++ terraform/layers/deploy/s3.tf
@@ -1,11 +1,20 @@
-resource "aws_s3_bucket" "codepipeline_artifcats" {
+resource "aws_s3_bucket" "codepipeline_artifacts" {
   bucket = "${var.environment}-${var.project}-codepipeline-bucket"
 }
 
 resource "aws_s3_bucket_versioning" "codepipeline_versioning" {
-  bucket = aws_s3_bucket.codepipeline_artifcats.bucket
+  bucket = aws_s3_bucket.codepipeline_artifacts.bucket
 
   versioning_configuration {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_ownership_controls" "copepipeline_ownership" {
+  bucket = aws_s3_bucket.codepipeline_artifacts.bucket
+
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
