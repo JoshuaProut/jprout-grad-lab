c89e119f09c69d7d270b1f240ab328047eacd6fd
Josh Prout <jnp207@exeter.ac.uk>
2024-12-17 13:41:04 +0000

    Add permissions for logging and artifacts bucket

--- terraform/layers/deploy/data.tf
+++ terraform/layers/deploy/data.tf
@@ -30,4 +30,30 @@ data "aws_iam_policy_document" "codepipeline_policy" {
       data.aws_codestarconnections_connection.codestar_connection.arn
     ]
   }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:List*",
+      "s3:Get*",
+      "s3:Put*",
+    ]
+
+    resources = [
+      aws_s3_bucket.codepipeline_artifacts.arn,
+      "${aws_s3_bucket.codepipeline_artifacts.arn}/*",
+    ]
+  }
 }
