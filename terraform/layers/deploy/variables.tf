81d6a466015c3aa1592e7782cb7b17d27f47cea4
Josh Prout <jnp207@exeter.ac.uk>
2024-12-17 10:33:48 +0000

    Create deploy layer

---
+++ terraform/layers/deploy/variables.tf
@@ -0,0 +1,17 @@
+variable "project" {
+  description = "The name of the project"
+  type        = string
+  default     = "grad-lab-1"
+}
+
+variable "environment" {
+  description = "The environment of the resource"
+  type        = string
+  default     = "dev"
+}
+
+variable "region" {
+  description = "Region for launch of resources"
+  type        = string
+  default     = "eu-west-2"
+}
