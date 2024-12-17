7041fd528f7eb34e63fe3c7ca281f4c29b36dcdd
Josh Prout <jnp207@exeter.ac.uk>
2024-12-17 14:25:31 +0000

    Create codepipeline

---
+++ terraform/layers/deploy/codepipeline.tf
@@ -0,0 +1,48 @@
+resource "aws_codepipeline" "codepipeline_deploy" {
+  name           = "${var.environment}-${var.project}-pipeline-deploy"
+  role_arn       = aws_iam_role.codepipeline_role.arn
+  pipeline_type  = "V2"
+  execution_mode = "QUEUED"
+
+  artifact_store {
+    location = aws_s3_bucket.codepipeline_artifacts.bucket
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "${var.github_org}-${var.github_repo}"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = 1
+      output_artifacts = ["source_tf_output"]
+      region           = var.region
+
+      configuration = {
+        ConnectionArn    = data.aws_codestarconnections_connection.codestar_connection.arn
+        FullRepositoryId = "${var.github_org}-${var.github_repo}"
+        BranchName       = var.github_branch
+      }
+    }
+  }
+
+  stage {
+    name = "DeployBase"
+    action {
+      name             = "BaseLayer"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["source_tf_output"]
+      output_artifacts = ["base_build_output"]
+      version          = 1
+
+      configuration = {
+        ProjectName = "github-${var.environment}-${var.project}-base"
+      }
+    }
+  }
+}
--- terraform/layers/deploy/variables.tf
+++ terraform/layers/deploy/variables.tf
@@ -1,3 +1,6 @@
+// Project --------------
+
+
 variable "project" {
   description = "The name of the project"
   type        = string
@@ -14,4 +17,25 @@ variable "region" {
   description = "Region for launch of resources"
   type        = string
   default     = "eu-west-2"
+}
+
+
+// Github -------------
+
+variable "github_org" {
+  description = "Github organisation name"
+  type        = string
+  default     = "JoshuaProut"
+}
+
+variable "github_repo" {
+  description = "Github repo name"
+  type        = string
+  default     = "jprout-grad-lab"
+}
+
+variable "github_branch" {
+  description = "Github branch to pull from"
+  type        = string
+  default     = "main"
 }
