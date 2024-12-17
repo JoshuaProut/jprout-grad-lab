af949218a16c6783668dd28272aa678e81b969c9
Josh Prout <jnp207@exeter.ac.uk>
2024-12-17 15:24:29 +0000

    Create basic codebuild resource

---
+++ terraform/layers/deploy/codebuild.tf
@@ -0,0 +1,20 @@
+resource "aws_codebuild_project" "codebuild_base" {
+
+  name = "${var.environment}-${var.project}-base-codebuild"
+
+  service_role = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image        = "aws/codebuild.standard:7.0"
+    type         = "LINUX_CONTAINER"
+  }
+
+  source {
+    type = "CODEPIPELINE"
+  }
+}
